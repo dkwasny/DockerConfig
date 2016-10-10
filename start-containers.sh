c1de772b6c7b9e7b08487819f814d3c4f0abf8ed
dkwasny <dkwasny67@gmail.com>
2016-10-10 02:27:38 +0000

    Since we're relying on DNS, there is no need for IPs

--- start-containers.sh
+++ start-containers.sh
@@ -4,19 +4,17 @@
 function run() {
 	IMAGE="$1";
 	NAME="$2";
-	IP="$3";
 
 	docker run \
 		-d \
 		--name "$NAME" \
 		-h "$NAME" \
 		--net kwasnet \
-		--ip "$IP" \
 		dkwasny/"$IMAGE";
 }
 
-run dnsmasq dnsmasq 172.18.1.1
-run hdfs-namenode hdfs-namenode 172.18.2.1
-run hdfs-secondary-namenode hdfs-secondary-namenode 172.18.2.2
-run hdfs-datanode hdfs-datanode-1 172.18.3.1
-run hdfs-datanode hdfs-datanode-2 172.18.3.2
+docker run -d --name dnsmasq -h dnsmasq --net kwasnet --ip 172.18.1.1 dkwasny/dnsmasq
+run hdfs-namenode hdfs-namenode;
+run hdfs-secondary-namenode hdfs-secondary-namenode;
+run hdfs-datanode hdfs-datanode-1;
+run hdfs-datanode hdfs-datanode-2;
