ab394e72a2ef5a28f682a0ffb3b1d4044b7186a6
dkwasny <dkwasny67@gmail.com>
2016-10-16 21:27:58 +0000

    Adding a volume for the client container
    Adding some configurability to start-daemons.sh as well.

--- start-client.sh
+++ start-client.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-docker run -t -i --name client -h client.docker --net docker dkwasny/client;
+BASE_DIR="$(cd $(dirname $0); echo $PWD)";
+
+docker run \
+	-t \
+	-i \
+	--name client \
+	-h client.docker \
+	--net docker \
+	-v $BASE_DIR/client-volume:/workspace/client-volume \
+	dkwasny/client;
--- start-daemons.sh
+++ start-daemons.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 function help() {
-	echo "$0 [-d NUM_DATANODES] [-r NUM_REGIONSERVERS] [-n NUM_NODEMANAGERS]";
-	echo "All server counts default to 1";
+	echo "$0 [-d NUM_DATANODES] [-r NUM_REGIONSERVERS] [-n NUM_NODEMANAGERS] [-a NUM_ALL]";
+	echo "NUM_ALL will apply to all other parameters";
 }
 
 function run() {
@@ -19,18 +19,34 @@ function run() {
 		dkwasny/"$IMAGE" > /dev/null;
 }
 
-NUM_DATANODES=1
-NUM_REGIONSERVERS=1
-NUM_NODEMANAGERS=1
-
-while getopts hd:r:n: ARG; do
+while getopts hd:r:n:a: ARG; do
 	case "$ARG" in
 		h) help; exit 0;;
 		d) NUM_DATANODES="$OPTARG";;
 		r) NUM_REGIONSERVERS="$OPTARG";;
 		n) NUM_NODEMANAGERS="$OPTARG";;
+		a) NUM_DATANODES="$OPTARG";
+			NUM_REGIONSERVERS="$OPTARG";
+			NUM_NODEMANAGERS="$OPTARG";;
 	esac;
 done;
+
+if [ -z "$NUM_DATANODES" ]; then
+	echo "NUM_DATANODES not specified";
+	exit 1;
+elif [ -z "$NUM_REGIONSERVERS" ]; then
+	echo "NUM_REGIONSERVERS not specified";
+	exit 1;
+elif [ -z "$NUM_NODEMANAGERS" ]; then
+	echo "NUM_NODEMANAGERS not specified";
+	exit 1;
+fi;
+
+echo "Starting with:
+	$NUM_DATANODES datanodes
+	$NUM_REGIONSERVERS regionservers
+	$NUM_NODEMANAGERS nodemanagers
+";
 
 echo "Starting dnsmasq";
 docker run -d --name "dnsmasq" -h "dnsmasq.docker" --net docker --ip 172.18.1.1 dkwasny/dnsmasq > /dev/null;
