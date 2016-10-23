4736b12c99fc649990614744b530db831eeefa8b
dkwasny <dkwasny67@gmail.com>
2016-10-23 17:43:26 +0000

    Going all-in on docker-compose
    My management scripts are starting to get a tad hairy.
    I'm still using docker-compose in bad ways (e.g. hardcoding hostnames
    and container names), but this is easier to manage than the management
    scripts.

--- stop-all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CONTAINERS_TO_STOP=$(docker ps -qa);
-if [ -z "$CONTAINERS_TO_STOP" ]; then
-	echo "No containers to stop";
-else
-	docker stop $CONTAINERS_TO_STOP;
-	docker rm $CONTAINERS_TO_STOP;
-fi;
